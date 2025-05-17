7508e413dcb5822fdfb66a86b4585f4647719e85
Karen Chibukhchyan <karen.chibuxchyan@marktguru.com>
2025-05-17 11:54:45 +0400

    Project is created and up

---
+++ K8SProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+
+var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(configuration).CreateLogger();
+
+try
+{
+    Log.Information("Hello world!");
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "There was a problem starting the application");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
