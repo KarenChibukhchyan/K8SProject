510bb0693a341b1b6bc674be5e5cc33e73eac4b3
Karen Chibukhchyan <karen.chibuxchyan@marktguru.com>
2025-05-17 17:21:14 +0400

    Changed top level to explicit Program class

--- src/K8SProject/Program.cs
+++ src/K8SProject/Program.cs
@@ -1,19 +1,25 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Serilog;
 
-var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
 
-Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(configuration).CreateLogger();
+        Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(configuration).CreateLogger();
 
-try
-{
-    Log.Information("Hello world!!!");
-}
-catch (Exception ex)
-{
-    Log.Fatal(ex, "There was a problem starting the application");
-}
-finally
-{
-    Log.CloseAndFlush();
+        try
+        {
+            Log.Information("Hello world!!!");
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "There was a problem starting the application");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
 }
